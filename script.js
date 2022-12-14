1eae12f01eea6ae443656ceaf2273490d8a73137
alun0511 <78014508+alun0511@users.noreply.github.com>
2022-12-14 20:36:05 +0100

    update js

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const body = document.querySelector("body");
 const aboutUsImage = document.querySelector(".about-us-image img");
 const logoFooter = document.querySelector(".logo-footer img");
 const hamburgerDiv = document.querySelector(".hamburger");
+const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
+const menuCloser = document.querySelector("close-menu");
 
-console.log(hamburgerDiv.classList);
+console.log(hamburgerIcon);
+
+hamburgerIcon.addEventListener("click", (event) => {
+  if (hamburgerDiv.classList.contains("hidden")) {
+    hamburgerDiv.classList.remove("hidden");
+  } else hamburgerDiv.classList.add("hidden");
+});
+hamburgerIcon.addEventListener("click", (event) => {
+  if (hamburgerDiv.classList.contains("hidden")) {
+    hamburgerDiv.classList.remove("hidden");
+  } else hamburgerDiv.classList.add("hidden");
+});
 
 // Create a media condition that targets viewports at least 768px wide
 

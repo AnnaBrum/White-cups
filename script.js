73ec9b3d7cb50618afcebef7537913bffb0df8b2
alun0511 <78014508+alun0511@users.noreply.github.com>
2022-12-14 16:19:02 +0100

    styling cards

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const body = document.querySelector("body");
 const aboutUsImage = document.querySelector(".about-us-image img");
+const logoFooter = document.querySelector(".logo-footer img");
+
+console.log(logoFooter.src);
 
 // Create a media condition that targets viewports at least 768px wide
-const mediaQuery = window.matchMedia("(min-width: 780px)");
+
 // Check if the media query is true
+
+const mediaQuery = window.matchMedia("(min-width: 780px)");
+
 if (mediaQuery.matches) {
   // Then trigger an alert
-
+  logoFooter.src = "images/desktop_logo.png";
   aboutUsImage.src = "images/aboutus_desktop.png";
-  console.log(aboutUsImage.src);
-} else aboutUsImage.src = "images/aboutus_mobile.png";
+} else {
+  logoFooter.src = "images/mobile_logo.png";
+  aboutUsImage.src = "images/aboutus_mobile.png";
+}

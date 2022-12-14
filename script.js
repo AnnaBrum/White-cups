bd99976e29889a4fe11b290a27c7782901dc5886
alun0511 <78014508+alun0511@users.noreply.github.com>
2022-12-14 23:06:54 +0100

    css + js + html
    finished hamburger
    fixed margins and padding
    added hover and active effects
    transitions and transforms

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const aboutUsImage = document.querySelector(".about-us-image img");
 const logoFooter = document.querySelector(".logo-footer img");
 const hamburgerDiv = document.querySelector(".hamburger");
 const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
-const menuCloser = document.querySelector("close-menu");
+const menuCloser = document.querySelector(".close-menu");
+const navIcons = document.querySelector(".nav-icons");
+const navIconsImg = navIcons.querySelectorAll("img");
+console.log(navIconsImg);
 
-console.log(hamburgerIcon);
+window.addEventListener("click", (event) => {
+  console.log(hamburgerDiv.classList);
+  if (
+    !hamburgerDiv.contains(event.target) &&
+    !hamburgerIcon.contains(event.target) &&
+    !menuCloser.contains(event.target) &&
+    !hamburgerDiv.classList.contains("hidden")
+  ) {
+    hamburgerDiv.classList.toggle("hidden");
+  }
+});
 
 hamburgerIcon.addEventListener("click", (event) => {
   if (hamburgerDiv.classList.contains("hidden")) {
@@ -13,11 +26,11 @@ hamburgerIcon.addEventListener("click", (event) => {
   } else hamburgerDiv.classList.add("hidden");
 });
 
-// hamburgerIcon.addEventListener("click", (event) => {
-//   if (hamburgerDiv.classList.contains("hidden")) {
-//     hamburgerDiv.classList.remove("hidden");
-//   } else hamburgerDiv.classList.add("hidden");
-// });
+menuCloser.addEventListener("click", (event) => {
+  if (hamburgerDiv.classList.contains("hidden")) {
+    hamburgerDiv.classList.remove("hidden");
+  } else hamburgerDiv.classList.add("hidden");
+});
 
 // Create a media condition that targets viewports at least 768px wide
 
@@ -26,10 +39,7 @@ hamburgerIcon.addEventListener("click", (event) => {
 const mediaQuery = window.matchMedia("(min-width: 780px)");
 
 if (mediaQuery.matches) {
-  // Then trigger an alert
-  logoFooter.src = "images/desktop_logo.png";
   aboutUsImage.src = "images/aboutus_desktop.png";
 } else {
-  logoFooter.src = "images/mobile_logo.png";
   aboutUsImage.src = "images/aboutus_mobile.png";
 }
